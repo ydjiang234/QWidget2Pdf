874b8924cbfcd272f82c7992cc18bb9cc526a2f9
Yadong Jiang <ydjiang234@gmail.com>
2018-07-02 16:17:27 +0100

    1

--- QWidget2Pdf.h
+++ QWidget2Pdf.h
@@ -1,11 +1,17 @@
 #ifndef QWIDGET2PDF_H
 #define QWIDGET2PDF_H
 
+#include <QWidget>
+#include <QPdfWriter>
+#include <QPainter>
+#include <iostream>
 
 class QWidget2Pdf
 {
 public:
     QWidget2Pdf();
+    ~QWidget2Pdf();
+    bool save(QWidget *w, QString path, int pageWidth=210);//page width should be in mm
 };
 
 #endif // QWIDGET2PDF_H
